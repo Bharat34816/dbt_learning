064880ae5f0408ce48b8a53da93e305a41d269b0
bharatpendyala3@gmail.com <bharatpendyala3@gmail.com>
2026-07-15 13:22:11 +0000

    order_items_cnt

---
+++ models/display_order_items_cnt.sql
@@ -0,0 +1,12 @@
+{{ config(
+    materialized='view'
+) }}
+
+
+
+
+select o.orderid as orderid,count(oi.orderitemid) as order_items_count
+from {{ source('landing', 'ordr') }} o
+join {{source('landing','ordritms')}} oi
+on o.orderid=oi.orderid
+group by o.orderid
